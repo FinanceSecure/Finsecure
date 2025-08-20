b9483125451208fd3b30bc85c89f5a6526746b12
ArthurSMA <arthursuassunamaiaalves@gmail.com>
2025-08-20 11:46:07 -0300

    adicionamento do saldoService

---
+++ services/saldoService.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { Platform } from "react-native";
+import { TokenService } from "./authService";
+
+interface SaldoResponse { 
+  id: string;
+  valor: number;
+  data: string;
+  atualizado: string;
+  usuarioId: string;
+}
+
+const apiUrl =
+  Platform.OS === "android"
+    ? "http://10.0.2.2:3000/api"
+    : "http://192.168.0.12:3000/api"
+
+const api = axios.create({
+  baseURL: `${apiUrl}`
+});
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = await TokenService.getToken()
+    if (token)
+      config.headers['Authorization'] = `Bearer ${token}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+)
+
+export const SaldoService = {
+  async verificarSaldo(): Promise<SaldoResponse> {
+    try {
+      const response = (await api.get("/saldo/verificar"));
+      return response.data;
+    } catch (err: any) {
+      console.error("Erro ao verificar o saldo:", err);
+      throw new Error(err)
+    }
+  },
+}
